b55e683a9606bc09965ce4e547f882621604bf69
sotair <krzys.sota@gmail.com>
2022-05-14 20:04:50 +0200

    (re)send body

--- app.go
+++ app.go
@@ -5,37 +5,17 @@ import (
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
+func debug(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Println("server: req", req)
-	body, err := req.GetBody()
-
-	if err == nil {
-		fmt.Fprintf(w, "%s", body)
-	} else {
-		fmt.Println("getBody Err", w, "err", err)
-
-	}
-
-	/*ctx := req.Context()
+	fmt.Println("server: req", req, "\nheader", req.Header, "\nbody", req.Body)
 	fmt.Println("server: hello handler started")
 	defer fmt.Println("server: hello handler ended")
-
-	select {
-	case <-time.After(4 * time.Second):
-		fmt.Fprintf(w, "%s", req.Context())
-	case <-ctx.Done():
-
-		err := ctx.Err()
-		fmt.Println("server:", err)
-		internalError := http.StatusInternalServerError
-		http.Error(w, err.Error(), internalError)
-	}*/
+	fmt.Fprintf(w, "%s", req.Body)
 }
 
 func main() {
 	fmt.Println("server: listening at \\")
 
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", debug)
 	http.ListenAndServe(":8090", nil)
 }
