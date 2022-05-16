08fcd8b609bf8b5d0d54e4f8bd6334929f95a755
sotair <krzys.sota@gmail.com>
2022-05-16 19:05:51 +0200

    ple

--- app.go
+++ app.go
@@ -2,30 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 )
 
 var counter int = 20
 
-func debug(w http.ResponseWriter, req *http.Request) {
+func handle(w http.ResponseWriter, req *http.Request) {
 
 	/*if counter%10 == 0 {
 		fmt.Println("server: req", req, "\nheader", req.Header, "\nbody", req.Body)
 	}
 	counter++*/
 	//if _, err := fmt.Fprintf(w, "%s", req.Body); err != nil {
-	defer func(w io.Writer, format string, a string) {
-		if _, err := fmt.Fprintf(w, format, a); err != nil {
-			fmt.Println("sending 'hej' error", err)
-		}
-	}(w, "%s", "hej")
+	if _, err := fmt.Fprintf(w, "hello"); err != nil {
+		fmt.Println("sending 'hej' error", err)
+	}
 }
 
 func main() {
-	fmt.Println("server: listening at \\")
-
-	http.HandleFunc("/", debug)
+	fmt.Println("server: listening at :8090/")
+	http.HandleFunc("/", handle)
 	log.Fatal(http.ListenAndServe(":8090", nil))
 }
