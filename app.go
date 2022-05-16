7f2c8148939a04befe0be5bc55ab1b136bd44fdb
sotair <krzys.sota@gmail.com>
2022-05-16 16:48:09 +0200

    no response

--- app.go
+++ app.go
@@ -9,14 +9,14 @@ var counter int = 20
 
 func debug(w http.ResponseWriter, req *http.Request) {
 
-	if counter%10 == 0 {
+	/*if counter%10 == 0 {
 		fmt.Println("server: req", req, "\nheader", req.Header, "\nbody", req.Body)
 	}
 	counter++
 	//defer fmt.Println("server: hello handler ended")
 	if _, err := fmt.Fprintf(w, "%s", req.Body); err != nil {
 		fmt.Println("sending body error", err)
-	}
+	}*/
 }
 
 func main() {
