3c35bed8742d48dc155857d3423ab55579310960
sotair <krzys.sota@gmail.com>
2022-05-23 15:04:57 +0200

    gin gonic

--- app.go
+++ app.go
@@ -1,27 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+import "github.com/gin-gonic/gin"
 
 var counter int = 20
 
-func handle(w http.ResponseWriter, req *http.Request) {
-
-	/*if counter%10 == 0 {
-		fmt.Println("server: req", req, "\nheader", req.Header, "\nbody", req.Body)
-	}
-	counter++*/
-	//if _, err := fmt.Fprintf(w, "%s", req.Body); err != nil {
+/*func handle(w http.ResponseWriter, req *http.Request) {
 	if _, err := fmt.Fprintf(w, "hello"); err != nil {
 		fmt.Println("sending 'hej' error", err)
 	}
-}
+}*/
 
 func main() {
-	fmt.Println("server: listening at :8090/")
+	/*fmt.Println("server: listening at :8090/")
 	http.HandleFunc("/", handle)
-	log.Fatal(http.ListenAndServe(":8090", nil))
+	log.Fatal(http.ListenAndServe(":8090", nil))*/
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8090") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
